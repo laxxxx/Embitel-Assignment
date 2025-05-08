cb663300908241da0c887ef58f9c9e3240c7e344
laxxxx <lakshmancool9@gmail.com>
2025-05-08 11:09:18 +0530

    added picker on a seperate vc as a dropdown

---
+++ ET-Assignment/Views/Controllers/CitySheetViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CitySheetViewController.swift
+//  ET-Assignment
+//
+//  Created by Sree Lakshman on 08/05/25.
+//
+
+import Foundation
+import UIKit
+
+class CitySheetViewController: UIViewController {
+    
+    var cities: [String] = []
+    var onCitySelected: ((String?) -> Void)?
+    
+    @IBOutlet weak var cityPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cityPicker.dataSource = self
+        cityPicker.delegate   = self
+    }
+}
+
+
+extension CitySheetViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int { 1 }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return cities.count + 1  // "All Cities" option
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return row == 0 ? "All Cities" : cities[row - 1]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCity = row == 0 ? nil : cities[row - 1]
+        onCitySelected?(selectedCity)
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ ET-Assignment/Views/Controllers/ListViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  ET-Assignment
+//
+//  Created by Sree Lakshman on 08/05/25.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    @IBOutlet weak var filterButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let viewModel = ListViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        bindViewModel()
+        viewModel.fetchUsers()
+    }
+    
+    func setupUI() {
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        let nib = UINib(nibName: "UserTableViewCell", bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: "UserTableViewCell")
+    }
+    
+    func bindViewModel() {
+        viewModel.onDataUpdate = { [weak self] in
+            self?.tableView.reloadData()
+        }
+    }
+    
+    @IBAction func didTapFilter(_ sender: UIButton) {
+        showBottomSheet()
+    }
+    
+    private func showBottomSheet() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        guard let bottomSheetVC = storyboard
+            .instantiateViewController(identifier: "CitySheetViewController")
+                as? CitySheetViewController
+        else { return }
+        
+        bottomSheetVC.cities = viewModel.cities
+        bottomSheetVC.onCitySelected = { [weak self] selectedCity in
+            self?.viewModel.filterUsers(by: selectedCity)
+            self?.filterButton.setTitle(selectedCity ?? "All Cities", for: .normal)
+        }
+        
+        if let sheet = bottomSheetVC.sheetPresentationController {
+            sheet.detents = [.medium(), .medium()]
+            sheet.prefersGrabberVisible = true
+            sheet.largestUndimmedDetentIdentifier = .large
+        }
+        
+        present(bottomSheetVC, animated: true)
+    }
+}
+
+extension ListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.filteredUsers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserTableViewCell", for: indexPath) as? UserTableViewCell
+        
+        let user = viewModel.filteredUsers[indexPath.row]
+        cell?.configure(with: user)
+        return cell!
+    }
+}
