59669ff20da9be95c7db0c6303b171b54a6bc713
laxxxx <lakshmancool9@gmail.com>
2025-05-08 09:56:33 +0530

    add picker and TableView

--- ET-Assignment/Views/ListViewController.swift
+++ ET-Assignment/Views/ListViewController.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ListViewController: UIViewController {
+    
+    @IBOutlet weak var cityPicker: UIPickerView!
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
